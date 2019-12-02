64bc878a616a3a3c3154598975ca4ee4f1eb57b8
Jungi <workingscorpion@gmail.com>
2019-12-02 18:14:42 +0900

    feat : 공지사항API apidoc 작업

--- src/routes/board.route.ts
+++ src/routes/board.route.ts
@@ -19,11 +19,88 @@ interface Board {
 }
 
 /**
- *
- * @api {get} /api/v1/admin/queryuser userlist
- * @apiName queryuser
- * @apiGroup Owners
- *
+ * @api {post} /api/v1/board/create 공지사항 작성 API
+ * @apiVersion 0.0.0
+ * @apiName board create
+ * @apiGroup scorpion
+ * @apiParam {object} body x-www-form-urlencoded 데이터
+ * @apiParamExample {json} Request-Example:
+ *     {
+ *      "title": "공지사항 제목입니다",
+ *      "description" : "공지사항 내용입니다",
+ *      "writer" : "admin@naver.com",
+ *      "writedate" : "19-11-12",
+ *      "fix" : true
+ *     }
+ * @apiSuccess {object} response DB에 저장된 데이터값
+ */
+/**
+ * @api {get} /api/v1/board/adlist 관리자용 공지사항 리스트 출력 API
+ * @apiVersion 0.0.0
+ * @apiName board admin list
+ * @apiGroup scorpion
+ * @apiSuccess {object} response 관리자용 공지사항 전체 출력
+ */
+
+/**
+ * @api {get} /api/v1/board/list 사용자용 공지사항 리스트 출력 API
+ * @apiVersion 0.0.0
+ * @apiName board list
+ * @apiGroup scorpion
+ * @apiSuccess {object} response 사용자용 공지사항 전체 출력
+ */
+
+/**
+ * @api {get} /api/v1/board/read/:id 공지사항 상세보기
+ * @apiVersion 0.0.0
+ * @apiName board read
+ * @apiGroup scorpion
+ * @apiParam {string} id 상세보기할 해당 공지사항 ObjectId
+ * @apiSampleUrl http://192.168.0.84:3002/api/v1/board/read/5de089d569296802f0982b44/
+ * @apiSuccess {object} response 조회한 ObjectId를 가진 공지사항 게시물 데이터
+ */
+
+/**
+ * @api {post} /api/v1/board/update/:id 공지사항 수정 API
+ * @apiVersion 0.0.0
+ * @apiName board update
+ * @apiGroup scorpion
+ * @apiParam {object} body x-www-form-urlencoded 데이터
+ * @apiParamExample {json} Request-Example:
+ *     {
+ *      "title": "수정된 공지사항 제목입니다",
+ *      "description" : "수정된 공지사항 내용입니다",
+ *      "writer" : "admin2@naver.com",
+ *      "writedate" : "19-11-20",
+ *      "fix" : false
+ *     }
+ * @apiSampleUrl http://192.168.0.84:3002/api/v1/board/update/5de089d569296802f0982b44/
+ * @apiSuccess {object} response 수정된 데이터값
+ */
+
+/**
+ * @api {delete} /api/v1/board/delete/:id 상세보기에서 해당 공지사항 삭제 API
+ * @apiVersion 0.0.0
+ * @apiName board delete one
+ * @apiGroup scorpion
+ * @apiSampleUrl http://192.168.0.84:3002/api/v1/board/delete/5de089d569296802f0982b44/
+ * @apiParam {string} id 삭제할 해당 공지사항 ObjecgId
+ */
+
+/**
+ * @api {delete} /api/v1/board/delete 2개 이상 공지사항 삭제 API
+ * @apiVersion 0.0.0
+ * @apiName board delete
+ * @apiGroup scorpion
+ * @apiParam {string} query 삭제할 공지사항 ObjectId 여러개
+ * @apiParam {string} query 삭제할 공지사항 ObjectId 여러개
+ * @apiParam {string} query 삭제할 공지사항 ObjectId 여러개
+ * @apiParamExample {string[]} Request-Example:
+ *     {
+ *      "id": "5de089d569296802f0982b44",
+ *      "id" : "5de089d569296802f0982b45",
+ *      "id" : "5de089d569296802f0982b46",
+ *     }
  */
 
 @route('/api/v1/board')
@@ -119,7 +196,7 @@ export default class BoardAPI implements MyDependencies {
     }
 
     //상세보기에서 삭제할 경우
-    @route('/delete/?id')
+    @route('/delete/:id')
     @DELETE()
     async deleteone(ctx: Koa.Context) {
         const params = ctx.params;
@@ -133,7 +210,7 @@ export default class BoardAPI implements MyDependencies {
 
     //리스트에서 삭제할 경우
     @route('/delete')
-    @GET()
+    @DELETE()
     async delete(ctx: Koa.Context) {
         const {id} = ctx.request.query;
         await this.dbService.performWithDB(async db => {
